3abee0a6442152364f500d9a41ef4c7476240a31
Oussema Ben Soltana <oussema.ben.soltana1@gmail.com>
2021-04-20 21:53:06 +0100

    backendheroku middlew

--- startup/routes.js
+++ startup/routes.js
@@ -34,6 +34,12 @@ const round = require("../routes/round");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static("public"));
