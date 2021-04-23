66f08e2cce79176ab1b55586b18d2b3ac0f3b498
Oussema Ben Soltana <oussema.ben.soltana1@gmail.com>
2021-04-23 03:59:33 +0100

    modify authoriz

--- middleware/authoriz.js
+++ middleware/authoriz.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-  if (!req.user.isAdmin)
+  if (req.user.isAdmin != 1)
     return res
       .status(403)
       .json({ message: "Access forbidden, you are not an admin user" });
