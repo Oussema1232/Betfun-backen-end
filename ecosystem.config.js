b5a94bbd9bb1c210949759ebf5d431a1a935d4ae
Oussema Ben Soltana <oussema.ben.soltana1@gmail.com>
2021-05-01 23:49:59 +0100

    ka3war

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,15 @@
 module.exports = {
   apps: [
     {
+      name: "app",
       script: "./app.js",
-      watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
-
-  deploy: {
-    production: {
-      user: "node",
-      host: "https://betfunbackend1.herokuapp.com",
-      ref: "origin/master",
-      repo: "https://git.heroku.com/betfunbackend1.git",
-      path: "https://betfunbackend1.herokuapp.com",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-    },
-  },
 };
